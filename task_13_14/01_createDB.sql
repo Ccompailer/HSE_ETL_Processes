a8ae0262c3969268dfc2c0109c017f937d44376a
Ccompailer <balabanov-03@list.ru>
2026-06-23 17:12:54 +0300

    Завершил задание 13 и 14

---
+++ task_13_14/01_createDB.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE IF NOT EXISTS transactions (
+    msno Utf8 NOT NULL,
+    payment_method_id Int32,
+    payment_plan_days Int32,
+    plan_list_price Int32,
+    actual_amount_paid Int32,
+    is_auto_renew Int32,
+    transaction_date Int32,
+    membership_expire_date Int32,
+    is_cancel Int32,
+    PRIMARY KEY (msno)
+);
